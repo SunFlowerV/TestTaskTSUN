9fbd01ad4d7fe4f767fff52e4afa21882c93133f
kirya <kirya@LAPTOP-D7B02M6N>
2022-12-22 07:56:09 +0300

    Добавьте файлы проекта.

---
+++ TestTaskTSUN/Field.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskTSUN
+{
+	public class Field
+	{
+		public string upperName { get; set; }
+
+		public string type { get; set; }
+		public bool isRequired { get; set; }
+		public bool isReadOnly { get; set; }
+		public bool isImmutable { get; set; }
+		public bool isMultiple { get; set; }
+		public bool isDynamic { get; set; }
+		public string title { get; set; }
+		public string listLabel { get; set; }
+		public string formLabel { get; set; }
+		public string filterLabel { get; set; }
+		public object settings { get; set; }
+
+	}
+}
---
+++ TestTaskTSUN/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Nodes;
+using TestTaskTSUN;
+
+using HttpClient client = new()
+{
+	BaseAddress = new Uri("https://pres.tsrealty.ru")
+};
+
+
+JsonNode root = await client.GetFromJsonAsync<JsonNode>("/testing.php");
+JsonNode result = root!["result"]!;
+JsonObject fildsObject = result!["fields"]!.AsObject();
+using var stream = new MemoryStream();
+using var writer = new Utf8JsonWriter(stream);
+fildsObject.WriteTo(writer);
+writer.Flush();
+Dictionary<string, Field> fieldsDictionary = JsonSerializer.Deserialize<Dictionary<string, Field>>(stream.ToArray());
+List<Field> fields = fieldsDictionary.Values.ToList();
+List<Field> enumerationFields = fields.Where(x => x.type == "enumeration").ToList();
+foreach (var enumerationField in enumerationFields)
+{
+	Guid uniqeName = Guid.NewGuid();
+	Console.WriteLine($"Поле: {uniqeName}, имеет название: {enumerationField.upperName} и является полем типа enumeration");
+	await SaveInDB(uniqeName);
+}
+
+async Task<string> SaveInDB(Guid uniqeName)
+{
+	Console.WriteLine($"Поле {uniqeName} добавлено в базу данных");
+	return "OK";
+}
+
